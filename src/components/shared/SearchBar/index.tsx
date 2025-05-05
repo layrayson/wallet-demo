9c999527b9608a3d1d3cc6e685af6f8a3478a6a7
layrayson <olalereazeez11@gmail.com>
2025-05-05 04:05:03 +0100

    feat: implement search bar

---
+++ src/components/shared/SearchBar/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, TextInput, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const SearchBar = () => {
+  return (
+    <View style={styles.container}>
+      <Icon name="search" size={20} color="#ccc" style={styles.icon} />
+      <TextInput
+        style={styles.input}
+        placeholder="Search"
+        placeholderTextColor="#ccc"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f7f7f7',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    height: 50,
+
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    height: 50,
+    color: '#333',
+    fontSize: 18,
+    fontWeight: 600,
+  },
+});
+
+export default SearchBar;
